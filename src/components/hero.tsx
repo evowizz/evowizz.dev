8daba40583d1680799a07a60013b215f4e62e7a8
Dylan Roussel <mail@evowizz.dev>
2024-04-07 20:46:36 +0200

    Add hover effect to hero text

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -1,36 +1,43 @@
-import { cn } from "@/lib/utils"
+import { cn } from '@/lib/utils'
 
 export const HeroText: React.FC<{
   className?: string
 }> = ({ className }) => {
   return (
-    <span className={cn('text-[calc(42px+38*(100vw-768px)/1024)] leading-tight font-600 select-none', className)}>
-      <Line className="motion-safe:animation-delay-200">
-        Crafting software,
-      </Line>
-      <Line className="motion-safe:animation-delay-400">
-        Shaping components &amp;
-      </Line>
-      <Line className="motion-safe:animation-delay-600">
-        Discovering hidden features
-      </Line>
+    <span
+      className={cn(
+        'text-[calc(42px+38*(100vw-768px)/1024)] leading-tight font-600 select-none',
+        className,
+      )}
+    >
+      <Line className="motion-safe:animation-delay-200">Crafting software,</Line>
+      <Line className="motion-safe:animation-delay-400">Shaping components &amp;</Line>
+      <Line className="motion-safe:animation-delay-600">Discovering hidden features</Line>
     </span>
   )
 }
 
-const Line = ({
-  className,
-  ...props
-}: React.HTMLAttributes<HTMLParagraphElement>) => (
-  <div>
-    <p
-      {...props}
-      className={cn(
-        'inline-block',
-        'motion-safe:opacity-0 motion-safe:animate-hero-appear',
-        'duration-300 ease-slow-in transition-colors',
-        className,
-      )}
-    />
-  </div>
-)
+const Line = ({ className, children, ...props }: React.HTMLAttributes<HTMLParagraphElement>) => {
+  if (typeof children !== 'string') throw new Error('children must be a string')
+  const letters = children.split('')
+
+  return (
+    <div>
+      <p
+        {...props}
+        className={cn(
+          'inline-block',
+          'motion-safe:opacity-0 motion-safe:animate-hero-appear',
+          'duration-300 ease-slow-in transition-colors',
+          className,
+        )}
+      >
+        {letters.map((letter, i) => (
+          <span key={i} className="inline-block hover-bubble">
+            {letter === ' ' ? '\u00A0' : letter}
+          </span>
+        ))}
+      </p>
+    </div>
+  )
+}
