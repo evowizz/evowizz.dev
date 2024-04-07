ac7c41dbf1ad1516d4d5ebfec0b31c067dc5011b
Dylan Roussel <mail@evowizz.dev>
2024-04-07 16:07:28 +0200

    Refactor Nav component to use NavLink component

--- src/components/header/nav.tsx
+++ src/components/header/nav.tsx
@@ -15,33 +15,43 @@ type NavProps = {
   destinations: NavDestination[]
 }
 
+type LinkProps = {
+  destination: NavDestination
+  isSelected: boolean
+}
+
+const NavLink = ({ destination: { path, label }, isSelected }: LinkProps) => (
+  <li key={path}>
+    <Link
+      href={path}
+      className={cn(
+        'group text-6xl hover:[font-stretch:150%] font-600 flex items-center opacity-70 transition-all',
+        isSelected && 'opacity-100',
+        !isSelected && 'hover:opacity-100 gap-2',
+      )}
+    >
+      {label}
+      <span
+        className={cn({
+          'transition-transform motion-safe:md:group-hover:translate-x-4': !isSelected,
+        })}
+      >
+        {isSelected ? <MiddleDot /> : <ArrowRight />}
+      </span>
+    </Link>
+  </li>
+)
+
 export const Nav = ({ destinations }: NavProps) => {
   const pathname = usePathname() ?? ''
   const isHome = pathname === '/'
 
   return (
     <nav>
-      <ul className="space-y-8">
+      <ul className="space-y-6">
         {destinations.map((nav) => {
           const isSelected = nav.path === '/' ? isHome : pathname.startsWith(nav.path)
-          return (
-            <li key={nav.path}>
-              <Link
-                href={nav.path}
-                className={cn('group text-5xl font-600 flex items-center', !isSelected && 'gap-2')}
-              >
-                {nav.label}
-                <span
-                  className={cn({
-                    'transition-transform motion-safe:md:group-hover:translate-x-2': !isSelected,
-                    'motion-safe:md:group-hover:animate-pulse': isSelected,
-                  })}
-                >
-                  {isSelected ? <MiddleDot /> : <ArrowRight />}
-                </span>
-              </Link>
-            </li>
-          )
+          return <NavLink key={nav.path} destination={nav} isSelected={isSelected} />
         })}
       </ul>
     </nav>
