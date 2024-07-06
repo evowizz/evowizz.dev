237f843508fcc308af658a2e2fb9ca8e513c2f9a
Dylan Roussel <mail@evowizz.dev>
2024-07-06 19:20:26 +0200

    Make noise background fixed

--- src/components/blob-background.tsx
+++ src/components/blob-background.tsx
@@ -5,5 +5,5 @@ type BlobBackgroundProps = {
 }
 
 export const BlobBackground = (props: BlobBackgroundProps) => {
-  return <div className="absolute inset-0 overlay-noise -z-[9999]" />
+  return <div className="fixed inset-0 overlay-noise -z-[9999]" />
 }
