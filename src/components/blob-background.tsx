8113011d609bf4b6b4fe424249a94d1dc957b97d
Dylan Roussel <mail@evowizz.dev>
2024-07-06 19:20:55 +0200

    Add TODO

--- src/components/blob-background.tsx
+++ src/components/blob-background.tsx
@@ -5,5 +5,6 @@ type BlobBackgroundProps = {
 }
 
 export const BlobBackground = (props: BlobBackgroundProps) => {
+  // TODO: Implement blob background
   return <div className="fixed inset-0 overlay-noise -z-[9999]" />
 }
