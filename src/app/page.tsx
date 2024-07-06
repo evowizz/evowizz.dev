eadb6e29d56cefa90e8c63f05a066543a758af5a
Dylan Roussel <mail@evowizz.dev>
2024-07-06 21:41:57 +0200

    Add About section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,9 @@
 import { HeroText } from '@/components/hero'
 import { BoxBlock } from '@/components/lego/box-block'
 import { StarShape } from '@/components/svg/star-shape'
+import avatar from './avatar.png'
+import Image from 'next/image'
+import Link from 'next/link'
 
 export default function Home() {
   return (
@@ -16,6 +19,42 @@ export default function Home() {
               />
             </div>
             <HeroText />
+          </div>
+        </BoxBlock>
+      </section>
+      <section id="about">
+        <BoxBlock className="flex flex-col justify-center items-center">
+          <div className="flex flex-col md:flex-row py-24 gap-4 max-w-screen-xl w-full">
+            <div className="flex flex-row gap-4 flex-1 shrink-0">
+              <div>
+                <Image
+                  src={avatar}
+                  alt="Wordmark &ldquo;evo&rdquo;"
+                  width={80}
+                  height={80}
+                  className="rounded-full aspect-square select-none"
+                />
+              </div>
+              <StarShape variant="quad" className="w-20 h-20" />
+            </div>
+            <div className="flex-1 shrink-0">
+              <h1 className="text-4xl font-800 mb-3">About</h1>
+              <p className="text-lg">
+                Non elit deserunt aliqua deserunt ipsum ullamco dolore duis cupidatat cillum.
+                Laboris laborum in cillum mollit deserunt. Tempor amet consequat culpa enim dolore
+                nisi. Aliquip eu consectetur eiusmod veniam. Esse proident sunt tempor ea irure
+                aliquip ea ipsum tempor minim ea deserunt cupidatat elit.
+              </p>
+            </div>
+          </div>
+        </BoxBlock>
+        <BoxBlock className="flex flex-col justify-center items-center">
+          <div className="flex flex-col py-24 gap-4 max-w-screen-xl w-full">
+            <div className="flex flex-col gap-4">
+              <StarShape variant="asterisk" className="w-20 h-20" />
+              <h1 className="text-4xl font-800 mb-3">Skills</h1>
+            </div>
+            {/* <SkillsGrid skills={skills} /> */}
           </div>
         </BoxBlock>
       </section>
