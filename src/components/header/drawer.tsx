15ca0bacd9bc56bf4ab6035ec16494229fce2a41
Dylan Roussel <mail@evowizz.dev>
2026-01-02 21:40:48 +0100

    Update drawer styling

--- src/components/header/drawer.tsx
+++ src/components/header/drawer.tsx
@@ -14,6 +14,7 @@ type DrawerProps = {
 }
 
 export const Drawer = ({ destinations, isOpen, closeDrawer }: DrawerProps) => {
+  // We're disabling the scroll on the body when the drawer is open.
   useEffect(() => {
     if (isOpen) {
       document.body.style.overflow = 'hidden'
@@ -30,7 +31,7 @@ export const Drawer = ({ destinations, isOpen, closeDrawer }: DrawerProps) => {
     <>
       <BoxBlock
         className={cn(
-          'bg-surface-container text-on-surface-variant fixed top-0 right-0 z-50 flex h-dvh w-full flex-col rounded-l-4xl md:w-1/2 xl:w-1/3',
+          'bg-surface-container-high text-on-surface-variant fixed top-0 right-0 z-50 flex h-dvh w-full flex-col md:rounded-l-4xl md:w-1/2 xl:w-1/3',
           'translate-x-full transform-gpu transition-transform duration-300 ease-out',
           isOpen && 'translate-x-0 transform',
         )}
@@ -56,8 +57,8 @@ export const Drawer = ({ destinations, isOpen, closeDrawer }: DrawerProps) => {
       <div
         className={cn(
           'pointer-events-none',
-          'bg-scrim fixed top-0 right-0 z-40 h-dvh w-full opacity-0 transition-opacity duration-300',
-          isOpen && 'pointer-events-auto opacity-75',
+          'bg-scrim/40 fixed top-0 right-0 z-40 h-dvh w-full opacity-0 backdrop-blur-sm transition-opacity duration-300',
+          isOpen && 'pointer-events-auto opacity-100',
         )}
         onClick={closeDrawer}
       />
