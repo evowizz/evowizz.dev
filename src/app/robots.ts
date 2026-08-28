0c058ad566b599c7c764ac5ae3e4922a45e1b6e4
Dylan Roussel <mail@evowizz.dev>
2026-08-28 15:22:29 +0200

    Disallow all inware paths in robots

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -6,7 +6,7 @@ export default function robots(): MetadataRoute.Robots {
     rules: {
       userAgent: '*',
       allow: '/',
-      disallow: '/inware/privacy_policy',
+      disallow: '/inware/',
     },
     sitemap: `${SITE_URL}/sitemap.xml`,
     host: SITE_URL,
